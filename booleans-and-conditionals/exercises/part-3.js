e68d424030751175cdb1a2be84858db387ac5316
Reginald Lamaute <reginaldjlamaute@gmail.com>
2024-01-10 15:48:27 -0500

    part 3 modified

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -17,6 +17,13 @@ e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndica
 f) Otherwise, print "Fuel and engine status pending..." */
 
 // Code 5a - 5f here:
+if (fuelLevel <= 5000 || engineTemperature > 2500) {
+    console.log("Check fuel level. Engines running hot");
+}
+if (fuelLevel <= 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking") {
+    console.log("ENGINE FAILURE IMMINENT!");
+}
+
 if (fuelLevel > 20000 && engineTemperature <= 2500) {
     console.log("Full tank. Engines good");
 }
@@ -26,12 +33,7 @@ if (fuelLevel > 10000 && engineTemperature <= 2500) {
 if (fuelLevel > 5000 && engineTemperature <= 2500) {
     console.log("Fuel level above 25%. Engines good.");
 }
-if (fuelLevel <= 5000 || engineTemperature > 2500) {
-    console.log("Check fuel level. Engines running hot");
-}
-if (fuelLevel <= 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking") {
-    console.log("ENGINE FAILURE IMMINENT!");
-}
+
 // 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
 let commandOverride = true;
 
