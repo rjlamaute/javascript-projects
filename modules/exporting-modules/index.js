63bd23b9207aa6d75210f34ed8a94b5129229fbc
Reginald Lamaute <reginaldjlamaute@gmail.com>
2024-02-03 14:26:16 -0500

    modules exercises

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,5 @@
 // Import the modules exported from practiceExports.js below:
+const practice = require("./practiceExports");
+
+console.log(typeof practice);
+console.log(practice.isPalindrome("that"));
