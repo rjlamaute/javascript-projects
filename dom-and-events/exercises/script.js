c4ef65bbd454bb56a219b406542ffa17e6001800
Reginald Lamaute <reginaldjlamaute@gmail.com>
2024-02-26 18:14:40 -0500

    DOM exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseleave', event => {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', function (event) {
+        const response = window.confirm("Are you sure you want to abort the mission?");
+        if(response) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+        }
+    })
+
 }
 
 window.addEventListener("load", init);
